1f39de4a8212c258f4c38a740443de8da3c4fd2b
junho1004 <dlwnsgh3619@naver.com>
2022-07-21 18:51:23 +0900

    lalala

---
+++ test_naver.js
@@ -0,0 +1,23 @@
+// const menus = document.querySelector("#NM_FAVORITE").textContent.split("\n");
+
+// const arr = [];
+
+// for (let i = 1; i <= 28; i++) {
+//   if (menus[i] != "    ") {
+//     arr.push(menus[i]);
+//   }
+// }
+// console.log(arr);
+
+// 위 방법으로 글자 앞 공백("    메일")을 지울 수 없어 아래 코드로 제출합니다.
+
+const menus = document.querySelectorAll(".nav");
+const menus_2 = document.querySelector(".btn_more");
+
+const arr = [];
+
+for (let i = 0; i < 16; i++) {
+  arr.push(menus[i].textContent);
+}
+arr.push(menus_2.textContent);
+console.log(arr);
